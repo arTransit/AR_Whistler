0e6fae5bba6a3ce34fc9cea4948ee9b07ad37246
Andrew Ross <andrew@fakePublicEmailAddress.ca>
2013-02-28 16:09:40 -0800

    GFI APC match on open/close door events

--- FME/Whistler.py
+++ FME/Whistler.py
@@ -1,9 +1,10 @@
 import fmeobjects
+import bisect
 
 """ Global Variables
 """
 # NEEDED_ATTRIBUTES=['APC{0}.LON','APC{0}.LAT','APC{0}.TIMESTAMP','APC{1}.LON','APC{1}.LAT','APC{1}.TIMESTAMP']
-LOGIT=0
+LOGIT=1
 
 
 class FMEmatchAPC(object):
@@ -22,21 +23,17 @@ class FMEmatchAPC(object):
         if f.getAttribute('_SOURCE') == 'GFI':
             self.gfiData.append( f )
         elif f.getAttribute('_SOURCE') == 'APC':
-            if f.getAttribute('BUSID') not in apcTimes.keys():
-                apcTimes[ f.getAttribute('BUSID') ] = []
-                apcIDs[ f.getAttribute('BUSID') ] = []
-            self.apcTimes[ f.getAttribute('BUSID') ].append(f.getAttribute('TIMESTAMP'))
-            self.apcIDs[ f.getAttribute('BUSID') ].append(f.getAttribute('TIMESTAMP'))
+            busID = int( f.getAttribute('BUSID') )
+            if busID not in self.apcTimes.keys():
+                self.apcTimes[ busID ] = []
+                self.apcIDs[ busID ] = []
+            self.apcTimes[ busID ].append( int(f.getAttribute('TIMESTAMP')) )
+            self.apcIDs[ busID ].append( int(f.getAttribute('_ID')) )
         else:
             self.logging("FMEmatchAPC: Unknown feature.")
 
 
     def close(self):
-        currentBus = 0
-        currentTime = 0
-        currentApc = self.apcData.pop(0)
-        lastApc = None
-        
         self.logging("FMEmatchAPC: Processing...")
         self.logging("FMEmatchAPC: gfiData %s" % len(self.gfiData) )
         
@@ -44,13 +41,15 @@ class FMEmatchAPC(object):
             self.logging("\nFMEmatchAPC: working on %s %s" % (
                     gfi.getAttribute( 'BUS' ),gfi.getAttribute( 'TIMESTAMP' )))
             
-            i = bisect.bisect_right( self.apcTimes[ gfi.getAttribute('BUSID') ], 
-                    gfi.getAttribute( 'TIMESTAMP' )) -1
+            busID = int( gfi.getAttribute('BUS') )
+            i = bisect.bisect_right( self.apcTimes[ busID ], 
+                    int( gfi.getAttribute( 'TIMESTAMP' )) ) -1
+            self.logging("\nFMEmatchAPC: index %d" % i)
             if i >=0 :
-                g.setAttribute( '_APCMATCHID', 
-                        self.apcIDs[ gfi.getAttribute('BUSID') ][i])
+                gfi.setAttribute( '_APCMATCHID', 
+                        self.apcIDs[ busID ][i])
             else :
-                g.setAttribute( '_APCMATCHID', 0)
+                gfi.setAttribute( '_APCMATCHID', 0)
             
             self.pyoutput( gfi )
 
